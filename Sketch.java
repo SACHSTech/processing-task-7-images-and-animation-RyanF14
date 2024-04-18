5da28edcdfaf833bf7888417ef2af932168d05e1
RyanF14 <ryan.fellin25@ycdsbk12.ca>
2024-04-18 10:39:57 -0400

    javadoc added

--- Sketch.java
+++ Sketch.java
@@ -1,8 +1,19 @@
 import processing.core.PApplet;
 import processing.core.PImage;
+/**
+ * Processing Task 7, animations, movement, images and collision detection
+ * @author: Ryan Fellin
+ * Sketch
+ * A recreation of the classic Commodore-Amiga "Boing! Ball" demo, an image of a grid is imported and drawn in as the background while a circle bounces around the screen
+ * This demo makes use of velocity to achieve the desired result of the illusion of gravity acting on the ball
+ * CircleX represents the x-position of the circle, circley represents the y position
+ * vely/velx represent the velocity of the circle in their respective directions
+ * radius defines the radius of the circle
+ */
+
 
 public class Sketch extends PApplet {
-	
+	//define variables
   float circley = 0;
   float circlex = 380;
   float vely = 3;
@@ -32,15 +43,16 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
+	//Generate background image
   PImage grid = loadImage("grid.png");
   background(grid);
-    PImage boing = loadImage("Boing_Ball.png");
+    //PImage boing = loadImage("Boing_Ball.png");
     
-
+    //draw circle
     ellipse(circlex,circley,radius*2,radius*2);
     circlex = circlex + velx;
     circley = circley + vely; 
+    //lower edge collision detection
     if(circley > height-90) {
       vely = (vely = (-20));
       
@@ -48,10 +60,10 @@ public class Sketch extends PApplet {
     else{
       vely = (vely +1 );
     }
+    //right/left collision detection
     if(circlex > width - 75 || circlex < 0 + 75){
       velx = (velx*(-1));
     }
   }
   
-  // define other methods down here.
 }
